979d2da09a2a40f50fbeffbf1ba8df442fb1df08
Rachel Grier <rgrier104@gmail.com>
2019-10-02 00:17:31 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -20,4 +20,11 @@ class Song < ActiveRecord::Base
     self.genre ? self.genre.name : nil
   end
 
+  def note_ids=(ids)
+  ids.each do |id|
+    note = Note.find(id)
+    self.notes << note
+  end
+end
+
 end
